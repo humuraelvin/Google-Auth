15763303da94501515d28c3eda540c94d9865717
humuraelvin <humura53@gmail.com>
2024-07-14 00:28:42 +0200

    changes made

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import Logo from '../../assets/7123025_logo_google_g_icon.png'
-import { signInWithGoogle } from '../../Firebase/Firebase.js'
-
-
-const buttonIsClicked = () => {
-    signInWithGoogle();
-}
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Logo from '../../assets/7123025_logo_google_g_icon.png';
+import { signInWithGoogle } from '../../Firebase/Firebase.js';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
 
+  const handleButtonClick = () => {
+    signInWithGoogle()
+      .then((result) => {
+        const user_name = result.user.displayName;
+        const user_email = result.user.email;
+        const profilePic = result.user.photoURL;
+
+        localStorage.setItem("name", user_name);
+        localStorage.setItem("email", user_email);
+        localStorage.setItem("profilePicture", profilePic);
+
+        console.log("Navigating to home...");
+        navigate('/home');
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  };
 
   return (
     <div>
-       <button onClick={buttonIsClicked} ><img src={Logo} alt="google" /> <h1>Sign In With Google</h1></button>
+      <button onClick={handleButtonClick}>
+        <img src={Logo} alt="google" />
+        <h1>Sign In With Google</h1>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
