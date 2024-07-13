156add8d81c5cb8eb6938737533211a10bf214b1
humuraelvin <humura53@gmail.com>
2024-07-13 23:03:13 +0200

    changes made

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -4,7 +4,7 @@ import Logo from '../../assets/7123025_logo_google_g_icon.png'
 const LoginPage = () => {
   return (
     <div>
-       <img src="" alt="" />
+       <button><img src={Logo} alt="google" /> <h1>Sign In With Google</h1></button>
     </div>
   )
 }
