a5a0a88a7e1b3ecc2978c6a8d7947ae16a38d844
humuraelvin <humura53@gmail.com>
2024-07-13 23:15:48 +0200

    changes made

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Logo from '../../assets/7123025_logo_google_g_icon.png'
+import { signInWithGoogle } from '../../Firebase/Firebase.js'
 
 const LoginPage = () => {
+
+    const buttonIsClicked = () => {
+        signInWithGoogle();
+    }
+
   return (
     <div>
-       <button><img src={Logo} alt="google" /> <h1>Sign In With Google</h1></button>
+       <button onClick={buttonIsClicked} ><img src={Logo} alt="google" /> <h1>Sign In With Google</h1></button>
     </div>
   )
 }
