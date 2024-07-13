c6d5d39776d11745f5061954f55528f5d66c1fd5
humuraelvin <humura53@gmail.com>
2024-07-14 00:29:18 +0200

    changes made

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,40 +1,36 @@
-import React from 'react'
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const name = localStorage.getItem("name");
+  const email = localStorage.getItem("email");
+  const profilePic = localStorage.getItem("profilePicture");
 
-    const name = localStorage.getItem("name");
-    const email = localStorage.getItem("email")
-    const profilePic = localStorage.getItem("profilePicture")
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const logout = () => {
-        navigate('/');
-    }
+  const logout = () => {
+    localStorage.clear();
+    navigate('/');
+  };
 
   return (
     <div>
       <h1 className='title'>Thank You, For You have successfully signed In</h1>
       <div className='details'>
         <div>
-            <h1>Your username is {name}</h1>
-            <br /><br />
-            <h1>Your email is {email}</h1>
+          <h1>Your username is {name}</h1>
+          <br /><br />
+          <h1>Your email is {email}</h1>
         </div>
         <div>
-            <img src={profilePic} alt="" />
+          <img src={profilePic} alt="" />
         </div>
       </div>
-
-        <div className='btn'>
-            <button onClick={logout}>
-                logout
-            </button>
-        </div>
-
+      <div className='btn'>
+        <button onClick={logout}>logout</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
