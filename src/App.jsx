d6bfc449424ee4ead0a080fe9c15057d515e1aa7
humuraelvin <humura53@gmail.com>
2024-07-14 00:28:56 +0200

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LoginPage from './components/LoginPage/LoginPage'
-import HomePage from './components/HomePage/HomePage'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage/LoginPage';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
-
   return (
-    <>
-     <BrowserRouter>
-        <Routes>
-          <Route path='/' exact element={<LoginPage />} />
-          <Route path='/home' exact element={<HomePage />} />
-        </Routes>
-     </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<LoginPage />} />
+        <Route path='/home' exact element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
