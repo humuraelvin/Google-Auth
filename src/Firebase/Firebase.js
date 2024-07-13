eda30995b4d8e64aff18719ec8061028cd4684d9
humuraelvin <humura53@gmail.com>
2024-07-13 22:34:40 +0200

    changes made

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,12 +1,15 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCb9f7vT5XZ7Mf8ntb64z4f1cdfa01owEo",
-  authDomain: "auth-ee867.firebaseapp.com",
-  projectId: "auth-ee867",
-  storageBucket: "auth-ee867.appspot.com",
-  messagingSenderId: "1079732236365",
-  appId: "1:1079732236365:web:c55fa66af0ff6b25176c04"
+  apiKey: "AIzaSyDZo3KUEL7KoL9K8WcvOL-w_Fxa8oI0zjQ",
+  authDomain: "auth-c34c6.firebaseapp.com",
+  projectId: "auth-c34c6",
+  storageBucket: "auth-c34c6.appspot.com",
+  messagingSenderId: "442871661499",
+  appId: "1:442871661499:web:c9e79141ee78b7c5a47a9e",
+  measurementId: "G-PWK4V3BSGZ"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
