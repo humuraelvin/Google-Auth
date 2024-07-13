e7a3f9f8dad8654b078350b57fa92b750456ba4c
humuraelvin <humura53@gmail.com>
2024-07-13 22:53:14 +0200

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './components/LoginPage/LoginPage'
+import HomePage from './components/HomePage/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <BrowserRouter>
+      
+     </BrowserRouter>
     </>
   )
 }
