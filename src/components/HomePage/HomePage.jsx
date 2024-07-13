7118f54c092679ca51df845f65c4476e5c1204bd
humuraelvin <humura53@gmail.com>
2024-07-13 23:37:52 +0200

    changes made

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const HomePage = () => {
+
+    const name = localStorage.getItem("name");
+    const email = localStorage.getItem("email")
+    const profilePic = localStorage.getItem("profilePicture")
+
   return (
     <div>
-      HomePage
+      <h1 className='title'>Thank You, For You have successfully Logged IN</h1>
+      <div className='details'>
+        <h1>Your username is {name}</h1>
+        <h1>Your email is {email}</h1>
+        <img src={profilePic} alt="" />
+      </div>
     </div>
   )
 }
